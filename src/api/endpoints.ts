cf277ff75ec39d8fbce3c0c6317a57cb2390f7c6
marned91 <152280051+marned91@users.noreply.github.com>
2025-09-02 19:33:20 +0200

    Create endpoints.ts

---
+++ src/api/endpoints.ts
@@ -0,0 +1,11 @@
+export const API_BASE = 'https://v2.api.noroff.dev';
+
+export const API_AUTH = `${API_BASE}/auth`;
+export const API_AUTH_LOGIN = `${API_AUTH}/login`;
+export const API_AUTH_REGISTER = `${API_AUTH}/register`;
+
+export const API_BOOKINGS = `${API_BASE}/holidaze/bookings`;
+export const API_VENUES = `${API_BASE}/holidaze/venues`;
+export const API_PROFILES = `${API_BASE}/holidaze/profiles`;
+
+export const API_KEY = '3fc37fe0-a306-47da-a608-cf5196d46e16';
