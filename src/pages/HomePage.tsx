5c5ed22538c7012d27cfc3be3dd87d506993b8c1
marned91 <152280051+marned91@users.noreply.github.com>
2025-09-18 16:14:02 +0200

    Hiding hero section for logged in users

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,13 @@
 import { HeroSection } from '../components/HeroSection';
 import { VenuesList } from '../components/VenuesHome/VenuesList';
+import { useAuthStatus } from '../hooks/useAuthStatus';
 
 export function HomePage() {
+  const { isLoggedIn } = useAuthStatus();
+
   return (
     <div>
-      <HeroSection />
+      {!isLoggedIn && <HeroSection />}
       <VenuesList />
     </div>
   );
